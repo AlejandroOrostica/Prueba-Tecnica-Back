b37062c7e23824fe195e4298a455591264144cef
Alejandro <alejandro.orostica@usach.cl>
2021-03-27 21:34:46 -0300

    Initial config finished

---
+++ backend/src/test/java/com/avla/test/TestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avla.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
