4793de37a53e9e54dd5fd95d73433e6f220ad6f4
Alejandro <alejandro.orostica@usach.cl>
2021-03-29 21:21:15 -0300

    email field is now unique in User model

--- backend/src/main/java/com/avla/test/models/User.java
+++ backend/src/main/java/com/avla/test/models/User.java
@@ -15,7 +15,7 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
-    @Column(name= "email")
+    @Column(name= "email", unique = true)
     private String email;
 
     @Column(name= "password")
